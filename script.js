22eb4e7723442bbcdec9e070e0eead1ffb6bbd0b
v-tadic <vukvukvukt@gmail.com>
2025-08-26 19:14:36 +0200

    Added the priceChangeColor function

--- script.js
+++ script.js
@@ -3,35 +3,49 @@ async function getData() {
         let response = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,solana,binancecoin,tether,ripple&vs_currencies=usd&include_market_cap=true&include_24hr_change=true');
         const data = await response.json();
         if(response.ok){
+
+            function priceChangeColor(elementId, value){
+                let roundValue = (Math.round(value * 100) / 100)
+                let displayValue = roundValue + '%';
+
+                document.getElementById(elementId).innerHTML = displayValue;
+
+                if(roundValue < 0){
+                    document.getElementById(elementId).style.color = 'red';
+                } else {
+                    document.getElementById(elementId).style.color = 'green';
+                }
+            }
+
             console.log(data);
             //bitcoin
             document.getElementById('BTCprice').innerHTML = (data.bitcoin.usd + ' $');
-            document.getElementById('BTCchange').innerHTML = (Math.round(data.bitcoin.usd_24h_change * 100) / 100 + ' $')
+            priceChangeColor('BTCchange', data.bitcoin.usd_24h_change);
             document.getElementById('BTCmarket').innerHTML =  (Math.round(data.bitcoin.usd_market_cap * 100) / 100 + ' $')
 
             //Etherium
             document.getElementById('ETHprice').innerHTML = (data.ethereum.usd + ' $');
-            document.getElementById('ETHchange').innerHTML = (Math.round(data.ethereum.usd_24h_change * 100) / 100 + ' $')
+            priceChangeColor('ETHchange', data.ethereum.usd_24h_change);
             document.getElementById('ETHmarket').innerHTML =  (Math.round(data.ethereum.usd_market_cap * 100) / 100 + ' $')
 
             //Solana
             document.getElementById('SOLprice').innerHTML = (data.solana.usd + ' $');
-            document.getElementById('SOLchange').innerHTML = (Math.round(data.solana.usd_24h_change * 100) / 100 + ' $')
+            priceChangeColor('SOLchange', data.solana.usd_24h_change);
             document.getElementById('SOLmarket').innerHTML =  (Math.round(data.solana.usd_market_cap * 100) / 100 + ' $')
 
             //XRP
             document.getElementById('XRPprice').innerHTML = (data.ripple.usd + ' $');
-            document.getElementById('XRPchange').innerHTML = (Math.round(data.ripple.usd_24h_change * 100) / 100 + ' $')
+            priceChangeColor('XRPchange', data.ripple.usd_24h_change);
             document.getElementById('XRPmarket').innerHTML =  (Math.round(data.ripple.usd_market_cap * 100) / 100 + ' $')
 
             //Tether
             document.getElementById('USDTprice').innerHTML = (data.tether.usd + ' $');
-            document.getElementById('USDTchange').innerHTML = (Math.round(data.tether.usd_24h_change * 100) / 100 + ' $')
+            priceChangeColor('USDTchange', data.tether.usd_24h_change);
             document.getElementById('USDTmarket').innerHTML =  (Math.round(data.tether.usd_market_cap * 100) / 100 + ' $')
 
             //BNB 
             document.getElementById('BNBprice').innerHTML = (data.binancecoin.usd + ' $');
-            document.getElementById('BNBchange').innerHTML = (Math.round(data.binancecoin.usd_24h_change * 100) / 100 + ' $')
+            priceChangeColor('BNBchange', data.binancecoin.usd_24h_change);
             document.getElementById('BNBmarket').innerHTML =  (Math.round(data.binancecoin.usd_market_cap * 100) / 100 + ' $')
 
         } else {
@@ -41,7 +55,9 @@ async function getData() {
         console.log(error)
     }
 }
- 
+
+
 getData();
 
 // setInterval(getData, 20000)
+
